2ac33052226d4b2f02f42c3a941ef9c9d4850675
Fleaux07 <romain.boucourt@gmail.com>
2026-03-19 22:01:13 +0100

    Création d'index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<h1>Salut fichierljflkqsj </h1>
+<?php
+$rssurl = "https://www.numerama.com/feed/";
+
+$rss = simplexml_load_file($rssurl);
+
+
+if ($rss) {
+    echo "<h1>Veilys : " . $rss->channel->title . "</h1>";
+    echo "<ul>";
+    $i = 0;
+
+    foreach ($rss->channel->item as $item) {
+        if ($i >= 5) break;
+        $title = $item->title;
+        $link = $item->link;
+        $date = date('d/m/Y', strtotime($item->pubDate));
+
+        echo "<li>";
+        echo "<strong><a href='$link' target='_blank'>$title</a></strong><br>";
+        echo "<em>Publié le $date</em>";
+        echo "</li><br>";
+
+        $i++;
+    }
+}
+
+
+echo "<ul>";
